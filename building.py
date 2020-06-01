31464af5f2cf7fa4057d66580de2016bcbe1fb35
jamesnitz <jamesanitz2@gmail.com>
2020-06-01 11:46:29 -0500

    urban planner 1

---
+++ building.py
@@ -0,0 +1,26 @@
+import datetime
+# URBAN PLANNER 1
+class Building:
+  def __init__(self, name, address, stories):
+    self.building_name = name
+    self.address = address
+    self.stories = stories
+    self.date_constructed = ""
+    self.owner = ""
+
+  def purchase(self, owner):
+    self.owner = owner
+  
+  def build(self):
+    self.date_constructed = datetime.datetime.now()
+  def report(self): 
+    print(f'{self.building_name} was purchased by {self.owner} on {self.date_constructed} and has {self.stories} stories.')
+    print()
+my_apt = Building('the shay', "9 City Place", 4)
+my_apt.purchase("Slim bankshot")
+my_apt.build()
+my_apt.report()
+runs_house = Building("church", "downtown", 2)
+runs_house.purchase("Ice Cole")
+runs_house.build()
+runs_house.report()
